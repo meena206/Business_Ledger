a4a512871a88196c3d5f40cec168b0d288465c7b
meena <meenachoudhary9680@gmail.com>
2026-09-24 21:14:28 +0530

    Fix allowed hosts for Railway

--- business_ledger/settings.py
+++ business_ledger/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-development-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","localhost,127.0.0.1").split(',')
+ALLOWED_HOSTS = ["localhost","127.0.0.1","businessledger-production.up.railway.app",]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
